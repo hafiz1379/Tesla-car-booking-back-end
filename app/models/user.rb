1b4f3062d4b9a1ca8081040111a3ed70bee4ea23
hafiz1379 <hafizrasa1379@gmail.com>
2023-11-30 19:26:44 +0430

    Add User model with Devise authentication and JWT support

---
+++ app/models/user.rb
@@ -0,0 +1,19 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+  include Devise::JWT::RevocationStrategies::JTIMatcher
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable,
+         :jwt_authenticatable, jwt_revocation_strategy: self
+
+  has_many :reservations, foreign_key: 'user_id', dependent: :destroy
+
+  # validations
+  validates :name, presence: true
+  validates :email, presence: true, uniqueness: true
+  validates :password, presence: true
+end
