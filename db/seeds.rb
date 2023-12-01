07e7092068a64ae4244af7132944bc6401701d5c
Bruno Kambere <brunokambere@gmail.com>
2023-12-01 18:36:29 +0200

    Add seeds for USer and Car

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,13 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+User.create([
+    {name: "Admin", email: "admin@gamil.com", password: "Admin@123456", role: "admin"},
+    {name: "User", email: "user@gamil.com", password: "User@123456", role: "user"}
+])
+
+Car.create([
+    {name: "Car 1", model: "Model 1", color: "Black", year: 2021, finance_fee: 1000, option_to_purchase: 2000, total_amount_payable: 3000, image: "https://www.audi.com/content/dam/gbp2/experience-audi/models-and-technology/production-models/a4/my2021/1920x1080-gal-prop-tx/1920x1080_desktop_A4_191001.jpg", duration: 12},
+    {name: "Car 2", model: "Model 2", color: "White", year: 2022, finance_fee: 1500, option_to_purchase: 2500, total_amount_payable: 3500, image: "https://www.bmw.com/content/dam/bmw/marketBMWCOM/bmw_com/categories/overview/3-series-sedan/2019/design/BMW-3-Series-Sedan-Design-Teaser-Desktop.jpg", duration: 10},
+])
