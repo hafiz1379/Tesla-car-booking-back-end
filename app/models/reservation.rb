709a0619f360360600e4ea665f5544f94b6f12b3
StevenWafeek <117098454+StevenWafeek@users.noreply.github.com>
2023-12-07 22:42:30 +0200

    Fix linters

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,6 +1,5 @@
 class Reservation < ApplicationRecord
-    belongs_to :user
-    belongs_to :car
-    validates :test_date, :city, presence: true
-    
+  belongs_to :user
+  belongs_to :car
+  validates :test_date, :city, presence: true
 end
