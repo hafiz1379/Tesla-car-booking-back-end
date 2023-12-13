4135903989cc0c90cc2fefe4a96503068b3c9f3c
StevenWafeek <117098454+StevenWafeek@users.noreply.github.com>
2023-12-13 23:12:03 +0200

    Update car finance_fee

--- app/models/car.rb
+++ app/models/car.rb
@@ -3,7 +3,7 @@ class Car < ApplicationRecord
 
   validates :name, presence: true
   validates :description, presence: true
-  validates :finance_fee, presence: true
+  validates :finance_fee, presence: true, numericality: { greater_than: 0 }
   validates :option_to_purchase, presence: true
   validates :total_amount_payable, presence: true
   validates :duration, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 1 }
