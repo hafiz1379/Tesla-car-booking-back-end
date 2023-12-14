fa25193289efaf54be92993dedc41c4182088f34
Iván Gonzalez <ivan.gonzalezr224@gmail.com>
2023-12-14 08:34:25 -0500

    Configure the app for deployment

--- Gemfile
+++ Gemfile
@@ -7,7 +7,7 @@ ruby '3.0.6'
 gem 'rails', '~> 7.1.2'
 
 # Use postgresql as the database for Active Record
-gem 'pg', '~> 1.1'
+gem 'pg', '~> 1.5', '>= 1.5.4'
 
 # Use the Puma web server [https://github.com/puma/puma]
 gem 'puma', '>= 5.0'
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -3,6 +3,7 @@ require "active_support/core_ext/integer/time"
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
+  config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present? || ENV['RENDER'].present?
   # Code is not reloaded between requests.
   config.enable_reloading = false
 
